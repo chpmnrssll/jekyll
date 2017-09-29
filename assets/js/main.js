ca55ba9aabc995457c30f4e987ac3823f7274e3d
chpmnrssll <chpmnrssll@gmail.com>
2017-09-29 17:54:29 -0600

    theme color picker w/localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,51 @@ $(".featured-items__article-summary").on("mouseleave", function(e) {
 });
 
 // Scroll to Top button
-$(".top").click(function() {
+$(".top").click(function () {
   $("html, body").stop().animate({ scrollTop: 0 }, "slow", "swing");
 });
+
+if(localStorage.getItem("hsl")) {
+  let themeColor = JSON.parse(localStorage.getItem("hsl"));
+  document.documentElement.style.setProperty("--hue", themeColor.h);
+  document.documentElement.style.setProperty("--saturation", themeColor.s + "%");
+  document.documentElement.style.setProperty("--lightness", themeColor.l + "%");
+}
+
+$("#themeColor").change(function () {
+  let color = hexToHSL(this.value);
+  localStorage.setItem("hsl", JSON.stringify({ h: color.h, s: color.s, l: color.l }));
+  document.documentElement.style.setProperty("--hue", color.h);
+  document.documentElement.style.setProperty("--saturation", color.s + "%");
+  document.documentElement.style.setProperty("--lightness", color.l + "%");
+});
+
+function hexToHSL(hex) {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  let r = parseInt(result[1], 16) / 255;
+  let g = parseInt(result[2], 16) / 255;
+  let b = parseInt(result[3], 16) / 255;
+  let max = Math.max(r, g, b);
+  let min = Math.min(r, g, b);
+  let h, s, l = (max + min) / 2;
+
+  if(max == min) {
+      h = s = 0; // achromatic
+  } else {
+      let d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch(max) {
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+  }
+
+  s = s*100;
+  l = l*100;
+  s = Math.round(s);
+  l = Math.round(l);
+  h = Math.round(360*h);
+  return { h: h, s: s, l: l };
+}
