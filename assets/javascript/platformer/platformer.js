b926a1bcbb84a857723aeda5fa6095d23763772d
chpmnrssll <chpmnrssll@gmail.com>
2018-03-02 14:17:42 -0700

    physics platformer

--- assets/javascript/platformer/platformer.js
+++ assets/javascript/platformer/platformer.js
@@ -10,28 +10,18 @@
 })()
 
 function init () {
-  let Engine = Matter.Engine
-  let Render = Matter.Render
-  let Runner = Matter.Runner
-  let Composites = Matter.Composites
-  let Common = Matter.Common
-  let MouseConstraint = Matter.MouseConstraint
-  let Mouse = Matter.Mouse
-  let World = Matter.World
-  let Bodies = Matter.Bodies
-
   // create engine
-  let engine = Engine.create()
+  let engine = Matter.Engine.create()
   let world = engine.world
   let canvas = document.querySelector('#platformer')
   let style = window.getComputedStyle(canvas)
 
   // create renderer
-  let render = Render.create({
+  let render = Matter.Render.create({
     canvas: canvas,
     engine: engine,
     options: {
-      background: '#0f0f13',
+      // background: '#0f0f13',
       width: parseInt(style.width),
       height: parseInt(style.height),
       showAngleIndicator: false,
@@ -39,81 +29,25 @@ function init () {
     }
   })
 
-  Render.run(render)
+  Matter.Render.run(render)
 
   // create runner
-  let runner = Runner.create()
-  Runner.run(runner, engine)
+  let runner = Matter.Runner.create()
+  Matter.Runner.run(runner, engine)
 
   // add bodies
   world.bodies = []
 
-  const width = render.options.width
-  const height = render.options.height
-  const wallSize = 10
+  Matter.World.add(world, screenBounds(render.options.width, render.options.height, 100))
 
-  // these static walls will not be rendered in this sprites example, see options
-  World.add(world, [
-    Bodies.rectangle(width / 2, 0, width * 2, wallSize, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, wallSize, height * 2, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width * 2, wallSize, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, wallSize, height * 2, { isStatic: true })
-  ])
-
-  let stack = Composites.stack(0, 0, 32, 16, width / 16, height / 16, (x, y) => {
-    let r = Common.random()
-    if (r > 0.5) {
-      return Bodies.rectangle(x, y, 58, 22, {
-        density: 0.5,
-        render: {
-          sprite: {
-            texture: '/assets/images/logo/logoNpm.png',
-            xScale: 0.25,
-            yScale: 0.25
-          }
-        }
-      })
-    } else if (r > 0.25) {
-      return Bodies.circle(x, y, 30, {
-        density: 0.005,
-        render: {
-          sprite: {
-            texture: '/assets/images/logo/logoAtom.png',
-            xScale: 0.25,
-            yScale: 0.25
-          }
-        }
-      })
-    } else if (r > 0.10) {
-      return Bodies.circle(x, y, 30, {
-        density: 0.25,
-        render: {
-          sprite: {
-            texture: '/assets/images/logo/logoYeoman.png',
-            xScale: 0.25,
-            yScale: 0.25
-          }
-        }
-      })
-    } else {
-      return Bodies.circle(x, y, 30, {
-        density: 0.05,
-        render: {
-          sprite: {
-            texture: '/assets/images/logo/logoBower.png',
-            xScale: 0.25,
-            yScale: 0.25
-          }
-        }
-      })
-    }
-  })
-
-  World.add(world, stack)
+  let w = parseInt(render.options.width / 8)
+  let h = parseInt(render.options.height / 8)
+  let stack = Matter.Composites.stack(0, 0, 4, 4, w, h, randomLogo)
+  Matter.World.add(world, stack)
 
   // add mouse control
-  let mouse = Mouse.create(render.canvas)
-  let mouseConstraint = MouseConstraint.create(engine, {
+  let mouse = Matter.Mouse.create(render.canvas)
+  let mouseConstraint = Matter.MouseConstraint.create(engine, {
     mouse: mouse,
     constraint: {
       stiffness: 0.2,
@@ -123,13 +57,13 @@ function init () {
     }
   })
 
-  World.add(world, mouseConstraint)
+  Matter.World.add(world, mouseConstraint)
 
   // keep the mouse in sync with rendering
   render.mouse = mouse
 
   // fit the render viewport to the scene
-  // Render.lookAt(render, {
+  // Matter.Render.lookAt(render, {
   //   min: {
   //     x: 0,
   //     y: 0
@@ -140,5 +74,86 @@ function init () {
   //   }
   // })
 
-  Engine.run(engine)
+  Matter.Engine.run(engine)
+}
+
+let lastRect = null
+let lastRound = null
+
+function randomLogo (x, y) {
+  const roundTextures = [
+    { density: 0.025, radius: 30, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoAMP.png' },
+    { density: 0.005, radius: 30, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoAtom.png' },
+    { density: 0.015, radius: 31, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoBower.png' },
+    { density: 0.05, radius: 29, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoChrome.png' },
+    { density: 0.015, radius: 24, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoWordpress.png' },
+    { density: 0.05, radius: 28, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoYeoman.png' }
+  ]
+
+  const rectTextures = [
+    { density: 0.7, width: 55, height: 64, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoGrunt.png' },
+    { density: 0.8, width: 29, height: 64, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoGulp.png' },
+    { density: 0.8, width: 38, height: 64, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoHTML5.png' },
+    { density: 0.4, width: 50, height: 50, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoJavascript.png' },
+    { density: 0.5, width: 60, height: 22, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoJekyll.png' },
+    { density: 0.6, width: 50, height: 52, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoMarionette.png' },
+    { density: 0.7, width: 64, height: 18, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoMongoDB.png' },
+    { density: 0.8, width: 62, height: 32, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoMySQL.png' },
+    { density: 0.6, width: 52, height: 32, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoNodeJS.png' },
+    { density: 0.8, width: 60, height: 21, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoNpm.png' },
+    { density: 0.7, width: 60, height: 32, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoPHP.png' },
+    { density: 0.7, width: 49, height: 62, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoRubyOnRails.png' },
+    { density: 0.6, width: 62, height: 48, xScale: 0.25, yScale: 0.25, url: '/assets/images/logo/logoSass.png' }
+  ]
+
+  let scale = Matter.Common.random(1, 1.5)
+
+  if (Matter.Common.random() > 0.5) {
+    let texture = rectTextures[parseInt(Matter.Common.random(0, rectTextures.length))]
+    while(lastRect === texture) {
+      texture = rectTextures[parseInt(Matter.Common.random(0, rectTextures.length))]
+    }
+    lastRect = texture
+    return Matter.Bodies.rectangle(x, y, texture.width * scale, texture.height * scale, {
+      density: texture.density * scale,
+      render: {
+        sprite: {
+          texture: texture.url,
+          xScale: texture.xScale * scale,
+          yScale: texture.yScale * scale
+        }
+      }
+    })
+  } else {
+    let texture = roundTextures[parseInt(Matter.Common.random(0, roundTextures.length))]
+    while(lastRound === texture) {
+      texture = roundTextures[parseInt(Matter.Common.random(0, roundTextures.length))]
+    }
+    lastRound = texture
+    return Matter.Bodies.circle(x, y, texture.radius * scale, {
+      density: texture.density * scale,
+      render: {
+        sprite: {
+          texture: texture.url,
+          xScale: texture.xScale * scale,
+          yScale: texture.yScale * scale
+        }
+      }
+    })
+  }
+}
+
+function screenBounds (width, height, size) {
+  const halfW = width / 2
+  const halfH = height / 2
+  const halfS = size / 2
+  const doubleW = width * 2
+  const doubleH = height * 2
+
+  return [
+    Matter.Bodies.rectangle(halfW, -halfS, doubleW, size, { isStatic: true }),
+    Matter.Bodies.rectangle(width + halfS, halfH, size, doubleH, { isStatic: true }),
+    Matter.Bodies.rectangle(halfW, height + halfS, doubleW, size, { isStatic: true }),
+    Matter.Bodies.rectangle(-halfS, halfH, size, doubleH, { isStatic: true })
+  ]
 }
