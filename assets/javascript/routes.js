9bd3b47f54f67d49b1d2f657f9f7372ceb19cbf6
chpmnrssll <chpmnrssll@gmail.com>
2018-03-18 01:16:25 -0600

    Fixed page load/exit transition animations, sequenced, simplified

--- assets/javascript/routes.js
+++ assets/javascript/routes.js
@@ -1,128 +1,129 @@
 const htmlStyles = window.getComputedStyle(document.querySelector('html'))
-const animationSpeed = parseInt(htmlStyles.getPropertyValue('--animation-speed').slice(0, -2)) * 2
+const animationSpeed = parseInt(htmlStyles.getPropertyValue('--animation-speed').slice(0, -2)) * 1
+const delaySpeed = 10
+const waitSpeed = 100
 
-// URL Request 'middleware' for all routes
+const easeInBack = 'cubic-bezier(0.600, -0.280, 0.735, 0.045)'
+const easeOutBack = 'cubic-bezier(0.175, 0.885, 0.320, 1.275)'
+const easeInOutBack = 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+
 page('*', (context, next) => {
-  // Ignore initial page load
-  if (!context.init) {
-    let request = new window.XMLHttpRequest()
-    request.onloadend = _loadHandler
-    request.open('GET', context.path, true)
-    request.send()
+  // Bail on initial page load
+  if (context.init) {
+    return
   }
+
+  let request = new window.XMLHttpRequest()
+  request.onloadend = event => {
+    if (event.target.status === 404) {
+      page('/404')
+    }
+
+    let parser = new window.DOMParser()
+    let newDocument = parser.parseFromString(event.target.response, 'text/html')
+    let newContent = newDocument.querySelector('.content')
+    let newArticle = newDocument.querySelector('.article')
+    let newFeatured = newDocument.querySelector('.featured')
+    let parent = newFeatured ? newFeatured : newArticle ? newArticle : newContent
+    let newElements = [].slice.call(parent.querySelectorAll('*'))
+
+    newElements.forEach(element => {
+      element.style.opacity = 0
+    })
+
+    let oldContent = document.querySelector('.content')
+    oldContent.parentNode.replaceChild(newContent, oldContent)
+    startAnyCanvasDemos()
+
+    newElements.map(element => {
+      element.depth = getElementDepth(newContent, element)
+      return element
+    }).sort((a, b) => {
+      return a.depth > b.depth ? 1 : -1
+    }).forEach((element, index) => {
+      attachElement(element, element.depth + index)
+    })
+  }
+
+  request.open('GET', context.path, true)
+  request.send()
 })
 
 page.exit('*', (context, next) => {
-  const pageTitle = document.querySelector('.header__title--main')
-  slide.out(pageTitle, animationSpeed, 1, 0).onfinish = () => {
-    pageTitle.remove()
-  }
+  let content = document.querySelector('.content')
+  let article = document.querySelector('.article')
+  let featured = document.querySelector('.featured')
+  let parent = featured ? featured : article ? article : content
+  let elements = [].slice.call(parent.querySelectorAll('*'))
 
-  const pageSubTitle = document.querySelector('.header__title--sub')
-  slide.out(pageSubTitle, animationSpeed, 1, 100).onfinish = () => {
-    pageSubTitle.remove()
-  }
-
-  const pageContent = document.querySelector('.content')
-  zoom.out(pageContent, animationSpeed, 1, 200).onfinish = () => {
-    while (pageContent.firstChild) {
-      pageContent.firstChild.remove()
+  elements.map(element => {
+    element.depth = getElementDepth(content, element)
+    return element
+  }).sort((a, b) => {
+    return a.depth < b.depth ? 1 : -1
+  }).forEach((element, index, list) => {
+    detachElement(element, element.depth + index)
+    if (index + 1 === list.length) {
+      setTimeout(next, (list.length * delaySpeed) + waitSpeed)
     }
-    setTimeout(next, animationSpeed * 2)
-  }
+  })
 })
 
 page()
+startAnyCanvasDemos()
 
-function _loadHandler (event) {
-  if (event.target.status === 404) {
-    page('/404')
-    // createStarField()
-  } else {
-    const parser = new window.DOMParser()
-    const newDocument = parser.parseFromString(event.target.response, 'text/html')
-    const newContent = newDocument.querySelector('.content')
-    const newSubTitle = newDocument.querySelector('.header__title--sub')
-    const newTitle = newDocument.querySelector('.header__title--main')
-    const pageContent = document.querySelector('.content')
-    const pageHeader = document.querySelector('.header__title')
-
-    newTitle.style.opacity = 0
-    pageHeader.appendChild(newTitle)
-    slide.in(newTitle, animationSpeed, 1, 0).onfinish = () => {
-      newTitle.style.opacity = 1
-    }
-
-    newSubTitle.style.opacity = 0
-    pageHeader.appendChild(newSubTitle)
-    slide.in(newSubTitle, animationSpeed, 1, 200).onfinish = () => {
-      newSubTitle.style.opacity = 1
-    }
-
-    newContent.style.opacity = 0
-    pageContent.parentNode.replaceChild(newContent, pageContent)
-    zoom.in(newContent, animationSpeed, 1, 400).onfinish = () => {
-      newContent.style.opacity = 1
-    }
-
-    // createStarField(pageContent)
+function startAnyCanvasDemos () {
+  const canvasDemo = document.querySelector('.canvasDemo')
+  if (canvasDemo) {
+    let req = new window.XMLHttpRequest()
+    req.addEventListener('load', event => {
+      if (window.demo) {
+        window.demo.stop()
+      }
+      eval(req.response)
+    })
+    req.open('GET', canvasDemo.getAttribute('src'), true)
+    req.send()
   }
 }
 
-const slide = {
-  in: (element, duration, iterations, delay) => {
-    const keyframes = [
-      { opacity: 0, transform: 'translate(-75vw, 0) scale(2.5, 0)' },
-      { opacity: 1, transform: 'none' }
-    ]
-    const timing = {
-      delay: delay,
-      duration: duration,
-      iterations: iterations,
-      easing: 'cubic-bezier(0.19, 1, 0.22, 1)'
-    }
-    return element.animate(keyframes, timing)
-  },
-  out: (element, duration, iterations, delay) => {
-    const keyframes = [
-      { opacity: 1, transform: 'none' },
-      { opacity: 0, transform: 'translate(-75vw, 0) scale(2.5, 0)' }
-    ]
-    const timing = {
-      delay: delay,
-      duration: duration,
-      iterations: iterations,
-      easing: 'cubic-bezier(0.19, 1, 0.22, 1)'
-    }
-    return element.animate(keyframes, timing)
+function getElementDepth (parent, descendant) {
+  let depth = 0
+  while (!descendant.isEqualNode(parent)) {
+    depth++
+    descendant = descendant.parentElement
   }
+
+  return depth
 }
 
-const zoom = {
-  in: (element, duration, iterations, delay) => {
-    const keyframes = [
-      { opacity: 0, transform: 'translate3d(-100vw, 0, -20vw) rotateY(-45deg)' },
-      { opacity: 1, transform: 'none' }
-    ]
-    const timing = {
-      delay: delay,
-      duration: duration,
-      iterations: iterations,
-      easing: 'cubic-bezier(0.19, 1, 0.22, 1)'
-    }
-    return element.animate(keyframes, timing)
-  },
-  out: (element, duration, iterations, delay) => {
-    const keyframes = [
-      { opacity: 1, transform: 'none' },
-      { opacity: 0, transform: 'translate3d(100vw, 0, -20vw) rotateY(45deg)' }
-    ]
-    const timing = {
-      delay: delay,
-      duration: duration,
-      iterations: iterations,
-      easing: 'cubic-bezier(0.19, 1, 0.22, 1)'
-    }
-    return element.animate(keyframes, timing)
+function attachElement (element, delay) {
+  const keyframes = [
+    { opacity: 0, transform: 'scale(0, 0)' },
+    { opacity: 1, transform: 'none' }
+  ]
+
+  transition(element, keyframes, animationSpeed, 1, delay, easeInOutBack)
+    .onfinish = () => { element.style.opacity = 1 }
+}
+
+function detachElement (element, delay) {
+  const keyframes = [
+    { opacity: 1, transform: 'none' },
+    { opacity: 0, transform: 'scale(0, 0)' }
+  ]
+
+  transition(element, keyframes, animationSpeed, 1, delay, easeInOutBack)
+    .onfinish = () => { element.style.opacity = 0 }
+}
+
+function transition (element, keyframes, duration, iterations, delay, easing) {
+  const timing = {
+    delay: delaySpeed * delay,
+    duration: duration,
+    iterations: iterations,
+    easing: easing
   }
+
+  return element.animate(keyframes, timing)
 }
