a86e1966b574b01b439adb616a098ced72508704
chpmnrssll <chpmnrssll@gmail.com>
2018-03-24 23:22:52 -0600

    upgrade to workbox 3.0.0

--- sw.js
+++ sw.js
@@ -1,30 +1,31 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js')
+this.importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js')
 
 if (workbox) {
   workbox.core.setLogLevel(workbox.core.LOG_LEVELS.silent)
 
   workbox.routing.registerRoute(
-    /.*\.(?:html|css|js)/,
-    // /.*\.*/,
-    // workbox.strategies.cacheFirst()
+    new RegExp('.*.(?:html|css|js)'),
     workbox.strategies.staleWhileRevalidate()
+    // workbox.strategies.networkFirst()
+    // workbox.strategies.cacheFirst()
   )
 
   workbox.routing.registerRoute(
-    /.*\.(?:png|jpg|jpeg|svg|gif)/,
-    workbox.strategies.cacheFirst()
+    new RegExp('.*.(?:png|jpg|jpeg|svg|gif)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // maxEntries: 256,
+          maxAgeSeconds: 30 * 24 * 60 * 60
+        })
+      ]
+    })
   )
 
   workbox.routing.registerRoute(
     new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'googleapis',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 30
-        })
-      ]
-    })
+    workbox.strategies.cacheFirst({ cacheName: 'googleapis' })
   )
 } else {
   console.log(`Boo! Workbox didn't load`)
