c8fe1b66587832e271f94726d2fcab7e119595cd
chpmnrssll <chpmnrssll@gmail.com>
2018-04-26 15:14:01 -0600

    taboola demo v3

--- test/loadRecommendations.js
+++ test/loadRecommendations.js
@@ -1,93 +1,116 @@
-function dataLoaded (json) {
-  const scale = 0.4
-  const pictureWidth = 600 * scale
-  const pictureHeight = 400 * scale
-  const logoWidth = 24
-  const logoHeight = 24
+class Recommendations {
+  constructor (uri, title, endpoint) {
+    this.scale = 0.4
+    this.pictureWidth = 600 * this.scale
+    this.pictureHeight = 400 * this.scale
+    this.logoWidth = 24
+    this.logoHeight = 24
 
-  let response = json.entries
-  // let response = JSON.parse(request.response).entries
+    // JSONP
+    // const callbackName = 'dataLoaded'
+    // let script = document.createElement('script')
+    // script.src = `${endpoint}?callback=${callbackName}&uri=${uri}&title=${title}`
+    // document.body.appendChild(script)
 
-  let container = document.querySelector('.recommendations__container')
-  container.style.border = '1px solid #ddd'
-  container.style.display = 'grid'
-  container.style.gridGap = '1em'
-  container.style.gridTemplateColumns = `repeat(auto-fill, minmax(${pictureWidth}px, 1fr))`
-  container.style.justifyItems = 'center'
-  container.style.overflow = 'auto'
-  container.style.padding = '1em'
-  container.style.resize = 'both'
+    // CORS
+    this.request = new window.XMLHttpRequest()
+    this.request.onload = this.dataLoaded
+    this.request.withCredentials = true
+    this.request.open('POST', endpoint, true)
+    this.request.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+    this.request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+    this.request.send({ uri: uri, title: title })
 
-  response.forEach((element, index) => {
-    let picture = document.createElement('img')
-    picture.src = element.pictureURI
-    picture.style.height = `${pictureHeight}px`
-    picture.style.width = `${pictureWidth}px`
+    this.container = document.querySelector('.recommendations__container')
+    Object.assign(this.container.style, {
+      border: '1px solid #ddd',
+      display: 'grid',
+      gridGap: '1em',
+      gridTemplateColumns: `repeat(auto-fill, minmax(${this.pictureWidth}px, 1fr))`,
+      justifyItems: 'center',
+      overflow: 'auto',
+      padding: '1em',
+      resize: 'both'
+    })
+  }
 
-    let headline = document.createElement('p')
-    headline.innerText = element.headline
-    headline.style.color = '#444'
-    headline.style.fontFamily = 'serif'
-    headline.style.margin = '0'
-    headline.style.padding = '.5em'
+  dataLoaded (json) {
+    // const response = json.entries
+    const response = JSON.parse(this.request.response).entries
 
-    let source = document.createElement('p')
-    source.innerText = element.source
-    source.style.color = '#999'
-    source.style.fontSize = '.8em'
-    source.style.margin = '0'
-    source.style.padding = '.5em'
+    response.forEach(entry => {
+      let picture = document.createElement('img')
+      picture.src = entry.pictureURI
+      Object.assign(picture.style, {
+        height: `${this.pictureHeight}px`,
+        width: `${this.pictureWidth}px`
+      })
 
-    let link = document.createElement('a')
-    link.href = element.linkURI
-    link.style.textDecoration = 'none'
-    link.style.width = `${pictureWidth}px`
+      let headline = document.createElement('p')
+      headline.innerText = entry.headline
+      Object.assign(headline.style, {
+        color: '#444',
+        fontFamily: 'serif',
+        margin: '0',
+        padding: '.5em'
+      })
 
-    link.appendChild(picture)
-    link.appendChild(headline)
-    link.appendChild(source)
-    container.appendChild(link)
-  })
+      let source = document.createElement('p')
+      source.innerText = entry.source
+      Object.assign(source.style, {
+        color: '#999',
+        fontSize: '.8em',
+        margin: '0',
+        padding: '.5em'
+      })
 
-  let img = document.createElement('img')
-  img.src = 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png'
-  img.style.height = `${logoHeight}px`
-  img.style.width = `${logoWidth}px`
-  img.style.opacity = '.5'
+      let link = document.createElement('a')
+      link.href = entry.linkURI
+      Object.assign(link.style, {
+        textDecoration: 'none',
+        width: `${this.pictureWidth}px`
+      })
 
-  let text = document.createElement('p')
-  text.innerText = 'Powered by OctoCats'
-  text.style.alignSelf = 'center'
-  text.style.fontSize = '.8em'
-  text.style.color = '#888'
+      link.appendChild(picture)
+      link.appendChild(headline)
+      link.appendChild(source)
+      this.container.appendChild(link)
+    })
 
-  let logo = document.createElement('div')
-  logo.style.alignItems = 'center'
-  logo.style.alignSelf = 'end'
-  logo.style.display = 'flex'
-  logo.style.gridColumnEnd = '-1'
-  logo.style.justifySelf = 'end'
+    let img = document.createElement('img')
+    img.src = 'https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png'
+    Object.assign(img.style, {
+      height: `${this.logoHeight}px`,
+      width: `${this.logoWidth}px`,
+      opacity: '.5'
+    })
 
-  logo.appendChild(img)
-  logo.appendChild(text)
-  container.appendChild(logo)
+    let text = document.createElement('p')
+    text.innerText = 'Powered by OctoCats'
+    Object.assign(text.style, {
+      alignSelf: 'center',
+      fontSize: '.8em',
+      color: '#888'
+    })
+
+    let logo = document.createElement('div')
+    Object.assign(logo.style, {
+      alignItems: 'center',
+      alignSelf: 'end',
+      display: 'flex',
+      gridColumnEnd: '-1',
+      justifySelf: 'end'
+    })
+
+    logo.appendChild(img)
+    logo.appendChild(text)
+    this.container.appendChild(logo)
+  }
 }
 
 (() => {
   const uri = encodeURIComponent(window.location)
   const title = encodeURIComponent(document.head.querySelector('title').innerText)
   const endpoint = 'https://widget.high.fi/silakka-json.cfm'
-  const callbackName = 'dataLoaded'
-
-  // let request = new window.XMLHttpRequest()
-  // request.onload = dataLoaded
-  // request.withCredentials = true
-  // request.open('POST', endpoint, true)
-  // request.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-  // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-  // request.send({ uri: uri, title: title })
-
-  let script = document.createElement('script')
-  script.src = `${endpoint}?callback=${callbackName}&uri=${uri}&title=${title}`
-  document.body.appendChild(script)
+  let r = new Recommendations(uri, title, endpoint)
 })()
